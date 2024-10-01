0b0ddbdabe50c0daa31a66e3d16533187e24fbe9
jntm <262448350@qq.com>
2024-10-01 19:05:06 +0800

    首页

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shop_dx_me/log/log_constanst.dart';
+import 'package:flutter_shop_dx_me/service/http_service.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,6 +21,62 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("1111"));
+    return Scaffold(
+      body: FutureBuilder(
+        future: request('homePageContext', formData: null),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            // myPrint(data.toString());
+
+            List<Map> swiperDataList =
+                (data['data']['slides'] as List).cast(); //轮播图
+            List<Map> navigatorList =
+                (data['data']['category'] as List).cast(); //分类
+            List<Map> recommendList =
+                (data['data']['recommend'] as List).cast(); //商品推荐
+            List<Map> floor1 = (data['data']['floor1'] as List).cast(); //底部商品推荐
+            Map fp1 = data['data']['floor1Pic']; //广告
+            myPrint("111 = $swiperDataList");
+            // myPrint("2222= $navigatorList");
+            // myPrint("333 = $floor1");
+            // myPrint("444 = $fp1");
+            return ListView(
+              children: [SwiperDiy(swiperDataList: swiperDataList)],
+            );
+          } else {
+            return Text("no data");
+          }
+        },
+      ),
+    );
+  }
+}
+
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+
+  const SwiperDiy({super.key, required this.swiperDataList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
+      child: Swiper(
+        itemCount: swiperDataList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            child: Image.network(
+              "${swiperDataList[index]['image']}",
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
   }
 }
